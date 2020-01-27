8ae52c9998e274debd5259e805a4fb291a15f6ad
Nestor Escoto <nescoto@Nestors-MacBook-Pro.local>
2020-01-27 18:16:11 -0500

    Adding For

--- tareas.go
+++ tareas.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type taskList struct {
+	tasks []*task
+}
+
+func (t *taskList) agregarALista(tl *task) {
+	t.tasks = append(t.tasks, tl)
+}
+
+func (t *taskList) eliminarDeLista(index int) {
+	t.tasks = append(t.tasks[:index], t.tasks[index+1:]...)
+}
+
 type task struct {
 	nombre      string
 	descripcion string
@@ -21,14 +33,38 @@ func (t *task) actualizarNombre(nombre string) {
 }
 
 func main() {
-	t := &task{
+	t1 := &task{
 		nombre:      "Completar mi curso de Go",
 		descripcion: "Completar mi curso de Go de Platzi en esta semana",
 	}
-	fmt.Printf("%+v\n", t)
-	t.marcarCompleta()
-	t.actualizarNombre("Finalizar mi curso de Go")
-	t.actualizarDescripcion("Completar mi curso cuanto antes")
-	fmt.Printf("%+v\n", t)
+
+	t2 := &task{
+		nombre:      "Completar mi curso de Python",
+		descripcion: "Completar mi curso de Python de Platzi en esta semana",
+	}
+
+	t3 := &task{
+		nombre:      "Completar mi curso de NodeJS",
+		descripcion: "Completar mi curso de NodeJS de Platzi en esta semana",
+	}
+
+	lista := &taskList{
+		tasks: []*task{
+			t1, t2,
+		},
+	}
+	lista.agregarALista(t3)
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			break
+		}
+		fmt.Println(i)
+	}
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			continue
+		}
+		fmt.Println(i)
+	}
 
 }

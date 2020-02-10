942b0b7b96b31abaf98b10a2351f634787c68089
Nestor Escoto <nescoto@Nestors-MacBook-Pro.local>
2020-02-10 15:01:58 -0600

    Adding Handlers to WebServer

---
+++ web_server/handlers.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func HandleHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the API Endpoint")
+}
+
+func PostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var metadata MetaData
+	err := decoder.Decode(&metadata)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Payload %v\n", metadata)
+}
+
+func UserPostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	response, err := user.ToJson()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
--- web_server/router.go
+++ web_server/router.go
@@ -1,6 +1,8 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type Router struct {
 	rules map[string]http.HandlerFunc
@@ -10,4 +12,26 @@ func NewRouter() *Router {
 	return &Router{
 		rules: make(map[string]http.HandlerFunc),
 	}
+}
+
+func (r *Router) FindHandler(path string, method string) (http.HandlerFunc, bool, bool) {
+	_, exist := r.rules[path]
+	handler, methodExist := r.rules[path][method]
+	return handler, methodExist, exist
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, request *http.Request) {
+	handler, methodExist, exist := r.FindHandler(request.URL.Path, request.Method)
+
+	if !exist {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	if !methodExist {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	handler(w, request)
 }
